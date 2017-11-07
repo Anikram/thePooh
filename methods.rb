5485f9d87d9e62567deb2377a5710299447a29e0
Anikram <4nikram@gmail.com>
2017-11-07 14:15:18 +0300

    Initial commit

---
+++ methods.rb
@@ -0,0 +1,9 @@
+def count_honeypots
+  honeypots = []
+
+  10000000.times do |i|
+  honeypots << "Honey pot # #{i}"
+  end
+
+end
+
---
+++ thePooh.rb
@@ -0,0 +1,15 @@
+#encoding: UTF-8
+
+require_relative('methods.rb')
+
+puts 'Winnie the Pooh is trying to sleep ... '
+
+count_honeypots
+
+GC.start
+
+
+
+gets
+
+
